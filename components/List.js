ecb7b0879c2a8059e0030b4f089727c9072a7b91
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-03-19 16:16:32 +0530

    refix

--- components/List.js
+++ components/List.js
@@ -4,7 +4,7 @@ const List = ({text,icon,color,src}) => {
     <li className=' h-16 flex justify-between -ml-32 hover:-ml-[10px] items-center  duration-300 '>
             
       <div className={`${color} absolute w-full h-16 filter blur opacity-80 z-0`}></div>
-          <a className='flex fixed justify-between items-center w-40 text-gray-300 z-10 p-2' href={src} target="_blank">
+          <a className='flex fixed justify-between items-center w-40 text-gray-300 z-10 p-2' href={src} target="_blank" rel="noreferrer">
                {text} {icon}
           </a>
     </li>
